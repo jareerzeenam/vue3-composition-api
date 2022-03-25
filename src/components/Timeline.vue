5104a254cfcdfb65e1373b0fa15411aa4815ee7c
jareer <jareerzeenam@adventus.io>
2022-03-25 14:35:06 +0530

    Initial commit

---
+++ src/components/Timeline.vue
@@ -0,0 +1,31 @@
+<template>
+<nav class="is-primary panel">
+  <span class="panel-tabs">
+    <a v-for="period in periods">
+      {{ period }}
+    </a>
+  </span>
+</nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Timeline",
+
+  setup(){
+    const periods = ['Today','This Week', 'This Month']
+
+    return{
+      periods
+    }
+  }
+
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
